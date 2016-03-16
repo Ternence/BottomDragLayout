b90edc03b218335fbcf782284e90cf302e3e7c40
zhangtengyuan <zhangtengyuan@163.com>
2016-03-16 16:52:58 +0800

    [master] change: Build Bottom Drag view

---
+++ app/src/main/java/com/tern/ui/bottomdraglayout/MainActivity.java
@@ -0,0 +1,80 @@
+package com.tern.ui.bottomdraglayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "DemoActivity";
+
+    private SlidingUpPanelLayout mLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lv = (ListView) findViewById(R.id.list);
+        List<String> your_array_list = Arrays.asList(
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view",
+                "This is a list view"
+        );
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list);
+
+        lv.setAdapter(arrayAdapter);
+
+        mLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                Log.i(TAG, "onPanelSlide, offset " + slideOffset);
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+                Log.i(TAG, "onPanelStateChanged " + newState);
+            }
+        });
+        mLayout.setFadeOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mLayout != null &&
+                (mLayout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED || mLayout.getPanelState() == SlidingUpPanelLayout.PanelState.ANCHORED)) {
+            mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
